218383fd4c8e464419285d3138421346fdb84890
Muhammad Shoaib Hafeez Ramay <shoaib@ganedata.co.uk>
2020-04-10 03:29:46 +0000

    Themes migrated to bootstrap 4 continue Imported from Changeset 2445

--- WarehouseEcommerce/App_Start/BundleConfig.cs
+++ WarehouseEcommerce/App_Start/BundleConfig.cs
@@ -9,21 +9,22 @@ namespace WarehouseEcommerce
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Areas/Shop/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Areas/Shop/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Areas/Shop/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Areas/Shop/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.bundle.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/app/css/styles").Include(
-                      "~/Areas/Shop/Content/bootstrap.css",
+                      "~/Content/bootstrap.css",
                       "~/Areas/Shop/Content/app/css/app.css",
                       "~/Areas/Shop/Content/site.css"
                       ));
