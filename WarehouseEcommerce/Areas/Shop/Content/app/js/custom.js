94e2e95f56ab134e25a90a685b208fe7139bf24b
umer <umer@ganedata.co.uk>
2020-04-21 21:16:59 +0000

    modify  grouped product page according to BS4 with responsive view on all screens Imported from Changeset 2464

--- WarehouseEcommerce/Areas/Shop/Content/app/js/custom.js
+++ WarehouseEcommerce/Areas/Shop/Content/app/js/custom.js
@@ -1,213 +1,254 @@
-﻿var fakewaffle = (function ($, fakewaffle) {
-    "use strict";
+﻿// heavily modified BS4 version of https://github.com/openam/bootstrap-responsive-tabs
+var fakewaffle = (function ($, fakewaffle) {
+	'use strict';
 
-    fakewaffle.responsiveTabs = function (collapseDisplayed) {
-        fakewaffle.currentPosition = "tabs";
+	fakewaffle.responsiveTabs = function (collapseDisplayed) {
 
-        var tabGroups = $(".nav-tabs.responsive");
-        var hidden = "";
-        var visible = "";
-        var activeTab = "";
+		fakewaffle.currentPosition = 'tabs';
 
-        if (collapseDisplayed === undefined) {
-            collapseDisplayed = ["xs", "sm"];
-        }
+		var tabGroups = $('.nav-tabs.responsive');
+		var hidden = '';
+		var visible = '';
+		var activeTab = '';
 
-        $.each(collapseDisplayed, function () {
-            hidden += " hidden-" + this;
-            visible += " visible-" + this;
-        });
+		// 		if ( collapseDisplayed === undefined ) {
+		// 			collapseDisplayed = [ 'xs', 'sm' ];
+		// 		}
 
-        $.each(tabGroups, function (index) {
-            var collapseDiv;
-            var $tabGroup = $(this);
-            var tabs = $tabGroup.find("li a");
+		// 		$.each( collapseDisplayed, function () {
+		// 			hidden  += ' banana-' + this;
+		// 			visible += ' visible-' + this;
+		// 		} );
 
-            if ($tabGroup.attr("id") === undefined) {
-                $tabGroup.attr("id", "tabs-" + index);
-            }
+		hidden = ' d-none d-sm-flex';
+		visible = ' d-sm-none';
 
-            collapseDiv = $("<div></div>", {
-                class: "panel-group responsive" + visible,
-                id: "collapse-" + $tabGroup.attr("id")
-            });
+		$.each(tabGroups, function (index) {
+			var collapseDiv;
+			var $tabGroup = $(this);
+			var tabs = $tabGroup.find('li a');
 
-            $.each(tabs, function () {
-                var $this = $(this);
-                var oldLinkClass =
-                    $this.attr("class") === undefined ? "" : $this.attr("class");
-                var newLinkClass = "accordion-toggle";
-                var oldParentClass =
-                    $this.parent().attr("class") === undefined
-                        ? ""
-                        : $this.parent().attr("class");
-                var newParentClass = "panel panel-default";
-                var newHash = $this.get(0).hash.replace("#", "collapse-");
+			if ($tabGroup.attr('id') === undefined) {
+				$tabGroup.attr('id', 'tabs-' + index);
+			}
 
-                if (oldLinkClass.length > 0) {
-                    newLinkClass += " " + oldLinkClass;
-                }
+			collapseDiv = $('<div></div>', {
+				'class': 'card-soup responsive' + visible,
+				'id': 'collapse-' + $tabGroup.attr('id')
+			});
 
-                if (oldParentClass.length > 0) {
-                    oldParentClass = oldParentClass.replace(/\bactive\b/g, "");
-                    newParentClass += " " + oldParentClass;
-                    newParentClass = newParentClass.replace(/\s{2,}/g, " ");
-                    newParentClass = newParentClass.replace(/^\s+|\s+$/g, "");
-                }
+			$.each(tabs, function () {
+				var $this = $(this);
+				var oldLinkClass = $this.attr('class') === undefined ? '' : $this.attr('class');
+				var newLinkClass = 'accordion-toggle';
+				var oldParentClass = $this.parent().attr('class') === undefined ? '' : $this.parent().attr('class');
+				var newParentClass = 'card';
+				var newHash = $this.get(0).hash.replace('#', 'collapse-');
 
-                if ($this.parent().hasClass("active")) {
-                    activeTab = "#" + newHash;
-                }
+				if (oldLinkClass.length > 0) {
+					newLinkClass += ' ' + oldLinkClass;
+				}
 
-                collapseDiv.append(
-                    $("<div>")
-                        .attr("class", newParentClass)
-                        .html(
-                            $("<div>")
-                                .attr("class", "panel-heading")
-                                .html(
-                                    $("<h4>")
-                                        .attr("class", "panel-title")
-                                        .html(
-                                            $("<a>", {
-                                                class: newLinkClass,
-                                                "data-toggle": "collapse",
-                                                "data-parent": "#collapse-" + $tabGroup.attr("id"),
-                                                href: "#" + newHash,
-                                                html: $this.html()
-                                            })
-                                        )
-                                )
-                        )
-                        .append(
-                            $("<div>", {
-                                id: newHash,
-                                class: "panel-collapse collapse"
-                            })
-                        )
-                );
-            });
+				if (oldParentClass.length > 0) {
+					oldParentClass = oldParentClass.replace(/\bactive\b/g, '');
+					newParentClass += ' ' + oldParentClass;
+					newParentClass = newParentClass.replace(/\s{2,}/g, ' ');
+					newParentClass = newParentClass.replace(/^\s+|\s+$/g, '');
+				}
 
-            $tabGroup.next().after(collapseDiv);
-            $tabGroup.addClass(hidden);
-            $(".tab-content.responsive").addClass(hidden);
+				if ($this.parent().hasClass('active')) {
+					activeTab = '#' + newHash;
+				}
 
-            if (activeTab) {
-                $(activeTab).collapse("show");
-            }
-        });
+				collapseDiv.append(
+					$('<div>').attr('class', newParentClass).html(
+						$('<div>').attr('class', 'card-header').html(
+							$('<h4>').attr('class', 'card-title').html(
+								$('<a>', {
+									'class': newLinkClass,
+									'data-toggle': 'collapse',
+									'data-parent': '#collapse-' + $tabGroup.attr('id'),
+									'href': '#' + newHash,
+									'html': $this.html()
+								})
+							)
+						)
+					).append(
+						$('<div>', {
+							'id': newHash,
+							'class': 'collapse'
+						})
+					)
+				);
+			});
 
-        fakewaffle.checkResize();
-        fakewaffle.bindTabToCollapse();
-    };
+			$tabGroup.next().after(collapseDiv);
+			$tabGroup.addClass(hidden);
+			$('.tab-content.responsive').addClass(hidden);
 
-    fakewaffle.checkResize = function () {
-        if (
-            $(".panel-group.responsive").is(":visible") === true &&
-            fakewaffle.currentPosition === "tabs"
-        ) {
-            fakewaffle.tabToPanel();
-            fakewaffle.currentPosition = "panel";
-        } else if (
-            $(".panel-group.responsive").is(":visible") === false &&
-            fakewaffle.currentPosition === "panel"
-        ) {
-            fakewaffle.panelToTab();
-            fakewaffle.currentPosition = "tabs";
-        }
-    };
+			if (activeTab) {
+				$(activeTab).collapse('show');
+			}
+		});
 
-    fakewaffle.tabToPanel = function () {
-        var tabGroups = $(".nav-tabs.responsive");
+		fakewaffle.checkResize();
+		fakewaffle.bindTabToCollapse();
+	};
 
-        $.each(tabGroups, function (index, tabGroup) {
-            // Find the tab
-            var tabContents = $(tabGroup)
-                .next(".tab-content")
-                .find(".tab-pane");
+	fakewaffle.checkResize = function () {
 
-            $.each(tabContents, function (index, tabContent) {
-                // Find the id to move the element to
-                var destinationId = $(tabContent)
-                    .attr("id")
-                    .replace(/^/, "#collapse-");
+		if ($('.card-soup.responsive').is(':visible') === true && fakewaffle.currentPosition === 'tabs') {
+			fakewaffle.tabToPanel();
+			fakewaffle.currentPosition = 'panel';
+		} else if ($('.card-soup.responsive').is(':visible') === false && fakewaffle.currentPosition === 'panel') {
+			fakewaffle.panelToTab();
+			fakewaffle.currentPosition = 'tabs';
+		}
 
-                // Convert tab to panel and move to destination
-                $(tabContent)
-                    .removeClass("tab-pane")
-                    .addClass("panel-body fw-previous-tab-pane")
-                    .appendTo($(destinationId));
-            });
-        });
-    };
+	};
 
-    fakewaffle.panelToTab = function () {
-        var panelGroups = $(".panel-group.responsive");
+	fakewaffle.tabToPanel = function () {
 
-        $.each(panelGroups, function (index, panelGroup) {
-            var destinationId = $(panelGroup)
-                .attr("id")
-                .replace("collapse-", "#");
-            var destination = $(destinationId).next(".tab-content")[0];
+		var tabGroups = $('.nav-tabs.responsive');
 
-            // Find the panel contents
-            var panelContents = $(panelGroup).find(
-                ".panel-body.fw-previous-tab-pane"
-            );
+		$.each(tabGroups, function (index, tabGroup) {
 
-            // Convert to tab and move to destination
-            panelContents
-                .removeClass("panel-body fw-previous-tab-pane")
-                .addClass("tab-pane")
-                .appendTo($(destination));
-        });
-    };
+			// Find the tab
+			var tabContents = $(tabGroup).next('.tab-content').find('.tab-pane');
 
-    fakewaffle.bindTabToCollapse = function () {
-        var tabs = $(".nav-tabs.responsive").find("li a");
-        var collapse = $(".panel-group.responsive").find(".panel-collapse");
+			$.each(tabContents, function (index, tabContent) {
+				// Find the id to move the element to
+				var destinationId = $(tabContent).attr('id').replace(/^/, '#collapse-');
 
-        // Toggle the panels when the associated tab is toggled
-        tabs.on("shown.bs.tab", function (e) {
-            if (fakewaffle.currentPosition === "tabs") {
-                var $current = $(e.currentTarget.hash.replace(/#/, "#collapse-"));
-                $current.collapse("show");
+				// Convert tab to panel and move to destination
+				$(tabContent)
+					.removeClass('tab-pane')
+					.addClass('card-body fw-previous-tab-pane')
+					.appendTo($(destinationId));
 
-                if (e.relatedTarget) {
-                    var $previous = $(e.relatedTarget.hash.replace(/#/, "#collapse-"));
-                    $previous.collapse("hide");
-                }
-            }
-        });
+			});
 
-        // Toggle the tab when the associated panel is toggled
-        collapse.on("shown.bs.collapse", function (e) {
-            if (fakewaffle.currentPosition === "panel") {
-                // Activate current tabs
-                var current = $(e.target).context.id.replace(/collapse-/g, "#");
-                $('a[href="' + current + '"]').tab("show");
+		});
 
-                // Update the content with active
-                var panelGroup = $(e.currentTarget).closest(".panel-group.responsive");
-                $(panelGroup)
-                    .find(".panel-body")
-                    .removeClass("active");
-                $(e.currentTarget)
-                    .find(".panel-body")
-                    .addClass("active");
-            }
-        });
-    };
+	};
 
-    $(window).resize(function () {
-        fakewaffle.checkResize();
-    });
+	fakewaffle.panelToTab = function () {
 
-    return fakewaffle;
-})(window.jQuery, fakewaffle || {});
+		var panelGroups = $('.card-soup.responsive');
+
+		$.each(panelGroups, function (index, panelGroup) {
+
+			var destinationId = $(panelGroup).attr('id').replace('collapse-', '#');
+			var destination = $(destinationId).next('.tab-content')[0];
+
+			// Find the panel contents
+			var panelContents = $(panelGroup).find('.card-body.fw-previous-tab-pane');
+
+			// Convert to tab and move to destination
+			panelContents
+				.removeClass('card-body fw-previous-tab-pane')
+				.addClass('tab-pane')
+				.appendTo($(destination));
+
+		});
+
+	};
+
+	fakewaffle.bindTabToCollapse = function () {
+
+		var tabs = $('.nav-tabs.responsive').find('li a');
+		var collapse = $('.card-soup.responsive').find('.card-collapse');
+
+		// Toggle the panels when the associated tab is toggled
+		tabs.on('shown.bs.tab', function (e) {
+
+			if (fakewaffle.currentPosition === 'tabs') {
+				var $current = $(e.currentTarget.hash.replace(/#/, '#collapse-'));
+				$current.collapse('show');
+
+				if (e.relatedTarget) {
+					var $previous = $(e.relatedTarget.hash.replace(/#/, '#collapse-'));
+					$previous.collapse('hide');
+				}
+			}
+
+		});
+
+		// Toggle the tab when the associated panel is toggled
+		collapse.on('shown.bs.collapse', function (e) {
+
+			if (fakewaffle.currentPosition === 'panel') {
+				// Activate current tabs
+				var current = $(e.target).context.id.replace(/collapse-/g, '#');
+				$('a[href="' + current + '"]').tab('show');
+
+				// Update the content with active
+				var panelGroup = $(e.currentTarget).closest('.card-soup.responsive');
+				$(panelGroup).find('.card-body').removeClass('active');
+				$(e.currentTarget).find('.card-body').addClass('active');
+			}
+
+		});
+	};
+
+	$(window).resize(function () {
+		fakewaffle.checkResize();
+	});
+
+	return fakewaffle;
+}(window.jQuery, fakewaffle || {}));
+
+fakewaffle.responsiveTabs();
 
 
 
-(function ($) {
-    fakewaffle.responsiveTabs(['xs']);
-})(jQuery);
+
+
+
+
+
+
+
+
+document.documentElement.setAttribute("lang", "en");
+document.documentElement.removeAttribute("class");
+
+axe.run(function (err, results) {
+	console.log(results.violations);
+});
+
+// Get IE or Edge browser version
+var version = detectIE();
+
+if (version !== false) {
+	alert('Please view in Chrome/Safari/Firefox');
+}
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+function detectIE() {
+	var ua = window.navigator.userAgent;
+
+	var msie = ua.indexOf('MSIE ');
+	if (msie > 0) {
+		// IE 10 or older => return version number
+		return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+	}
+
+	var trident = ua.indexOf('Trident/');
+	if (trident > 0) {
+		// IE 11 => return version number
+		var rv = ua.indexOf('rv:');
+		return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+	}
+
+	var edge = ua.indexOf('Edge/');
+	if (edge > 0) {
+		// Edge (IE 12+) => return version number
+		return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+	}
+
+	// other browser
+	return false;
+}
