63d6be51706ac1f2782c3215b00f41984bd3785f
umer <umer@ganedata.co.uk>
2020-05-21 16:10:26 +0000

    remove UniversityCustomStyle.css and add new new files Uni_Site.css and Uni_App.css Imported from Changeset 2529

--- WarehouseEcommerce/App_Start/BundleConfig.cs
+++ WarehouseEcommerce/App_Start/BundleConfig.cs
@@ -30,9 +30,8 @@ namespace WarehouseEcommerce
                       ));
             bundles.Add(new StyleBundle("~/Content/app/css/styles/university").Include(
                      "~/Content/bootstrap.css",
-                      "~/Content/Theme/Smart/app/css/app.css",
-                      "~/Content/Theme/Smart/site.css",
-                      "~/Content/Theme/University/UniversityCustomStyle.css",
+                      "~/Content/Theme/University/app/css/Uni_App.css",
+                       "~/Content/Theme/University/app/css/Uni_Site.css",
                      "~/Content/Theme/University/app/css/northumbria_custom.css",
                      "~/Content/Theme/University/app/css/customfontIcons.css",
                       "~/Content/Theme/University/app/css/skew_carousle_style.css",
