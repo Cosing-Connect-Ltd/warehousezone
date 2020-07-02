ca272ed9a432ff273864a836a4d64a2842a37a17
Naveed Tar <naveed_tariq1992@outlook.com>
2020-07-02 12:00:33 +0000

    sessionkey and null abel userid added in migartion Imported from Changeset 2626

---
+++ GaneEcommerce.Data/Migrations/202007020929298_sessionkey-added-in-websitecartitem.cs
@@ -0,0 +1,18 @@
+﻿namespace Ganedata.Core.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class sessionkeyaddedinwebsitecartitem : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.WebsiteCartItems", "SessionKey", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.WebsiteCartItems", "SessionKey");
+        }
+    }
+}
---
+++ GaneEcommerce.Data/Migrations/202007021028069_setuserid-nullable-inCartitems.cs
@@ -0,0 +1,22 @@
+﻿namespace Ganedata.Core.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class setuseridnullableinCartitems : DbMigration
+    {
+        public override void Up()
+        {
+            DropIndex("dbo.WebsiteCartItems", new[] { "UserId" });
+            AlterColumn("dbo.WebsiteCartItems", "UserId", c => c.Int());
+            CreateIndex("dbo.WebsiteCartItems", "UserId");
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.WebsiteCartItems", new[] { "UserId" });
+            AlterColumn("dbo.WebsiteCartItems", "UserId", c => c.Int(nullable: false));
+            CreateIndex("dbo.WebsiteCartItems", "UserId");
+        }
+    }
+}
