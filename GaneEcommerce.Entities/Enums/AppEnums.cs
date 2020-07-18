a2e403fbf21b89c07bfcac30293d51e6ad335682
umer <umer@ganedata.co.uk>
2020-07-18 18:44:57 +0000

    fix UI bugs from home page Imported from Changeset 2870

--- GaneEcommerce.Entities/Enums/AppEnums.cs
+++ GaneEcommerce.Entities/Enums/AppEnums.cs
@@ -360,9 +360,9 @@ namespace Ganedata.Core.Entities.Enums
     }
     public enum SortProductTypeEnum
     {
-        [Display(Name = "Name(A-Z)")]
+        [Display(Name = "Name (A-Z)")]
         NameByAsc = 1,
-        [Display(Name = "Name(Z-A)")]
+        [Display(Name = "Name (Z-A)")]
         NameByDesc = 2,
         [Display(Name = "Price (High > Low)")]
         PriceByDesc = 3,
