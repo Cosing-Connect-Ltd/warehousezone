a591eb2bb6170955fdc3a7d92d568c5fc5e4eacc
Sattar <sattar@ganedata.co.uk>
2020-08-07 14:30:06 +0100

    added product attribute and categories association import feature.

---
+++ GaneEcommerce.Entities/Domain/ImportModels/ProductsCategoriesAssociationsImportModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Ganedata.Core.Entities.Domain.ImportModels
+{
+    public class ProductsCategoriesAssociationsImportModel
+    {
+
+        public string ParentHeading { get; set; }
+        public string ChildHeading { get; set; }
+        public string SubChildHeading { get; set; }
+        public string SkuCode { get; set; }
+    }
+}
