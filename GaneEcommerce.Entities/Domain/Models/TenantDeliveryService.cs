1e8ff23e52556a290fd3f6ebe48864ec30445e0c
Sattar <sattar@ganedata.co.uk>
2020-10-22 15:39:25 +0100

    fixed orders load bug in some scenarios

--- GaneEcommerce.Entities/Domain/Models/TenantDeliveryService.cs
+++ GaneEcommerce.Entities/Domain/Models/TenantDeliveryService.cs
@@ -1,20 +1,18 @@
 ﻿using Ganedata.Core.Entities.Enums;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace Ganedata.Core.Entities.Domain
 {
+    [Serializable]
     public class TenantDeliveryService : PersistableEntity<int>
     {
         public int Id { get; set; }
         public string NetworkCode { get; set; }
         public string NetworkDescription { get; set; }
         public DeliveryMethods DeliveryMethod { get; set; }
-       
 
-        
+
+
 
     }
 }
