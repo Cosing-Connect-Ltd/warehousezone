4077362794af0c5e8ab8de1a7cfeb88cad3ccc76
Muhammad Shoaib Hafeez Ramay <shoaib@ganedata.co.uk>
2020-10-29 00:00:14 +0000

    FoodOrderType changed to nullable

---
+++ GaneEcommerce.Data/Migrations/202010282251544_FoodOrderType-nullable-in-Orders.cs
@@ -0,0 +1,18 @@
+﻿namespace Ganedata.Core.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FoodOrderTypenullableinOrders : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Orders", "FoodOrderType", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Orders", "FoodOrderType", c => c.Int(nullable: false));
+        }
+    }
+}
