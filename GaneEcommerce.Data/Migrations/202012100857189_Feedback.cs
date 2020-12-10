be1b9634f301bbcdcaa2ea5f34b5032d7d9d11de
Riyaz <riyaz@ganedata.co.uk>
2020-12-10 17:06:42 +0530

    Feedback apis

---
+++ GaneEcommerce.Data/Migrations/202012100857189_Feedback.cs
@@ -0,0 +1,41 @@
+﻿namespace Ganedata.Core.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Feedback : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Feedbacks",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        AccountID = c.Int(nullable: false),
+                        OrderID = c.Int(nullable: false),
+                        ServiceRate = c.String(),
+                        FoodRate = c.String(),
+                        AppRate = c.String(),
+                        FeedbackMessge = c.String(),
+                        TenantId = c.Int(nullable: false),
+                        IsDeleted = c.Boolean(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Account", t => t.AccountID)
+                .ForeignKey("dbo.Orders", t => t.OrderID)
+                .Index(t => t.AccountID)
+                .Index(t => t.OrderID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Feedbacks", "OrderID", "dbo.Orders");
+            DropForeignKey("dbo.Feedbacks", "AccountID", "dbo.Account");
+            DropIndex("dbo.Feedbacks", new[] { "OrderID" });
+            DropIndex("dbo.Feedbacks", new[] { "AccountID" });
+            DropTable("dbo.Feedbacks");
+        }
+    }
+}
---
+++ GaneEcommerce.Services/Feedbacks/IFeedbackService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ganedata.Core.Entities.Domain.Feedback;
+
+namespace Ganedata.Core.Services.Feedbacks
+{
+    public interface IFeedbackService
+    {
+        IEnumerable<Feedback> GetAll(int tenantId);
+        bool Create(Feedback feedback);
+    }
+}
