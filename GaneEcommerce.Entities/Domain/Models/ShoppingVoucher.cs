c66c4d17a224bcbfd82f79f27a5acb36b9b3237f
Riyaz Hameed <riyash.ips@live.com>
2021-01-13 21:40:32 +0000

    Voucher codes in progress - staging released

---
+++ GaneEcommerce.Entities/Domain/Models/ShoppingVoucher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Ganedata.Core.Entities.Enums;
+
+namespace Ganedata.Core.Entities.Domain
+{
+    public class ShoppingVoucher : BaseAuditInfo
+    {
+        [Key]
+        [Display(Name = "Voucher Id")]
+        public int ShoppingVoucherId { get; set; }
+        [Display(Name = "Voucher Title")]
+        public string VoucherTitle { get; set; }
+        [Display(Name = "Voucher Code")]
+        public string VoucherCode { get; set; }
+
+        [Display(Name = "User (If voucher is user specific)")]
+        public int? VoucherUserId { get; set; }
+        [ForeignKey("VoucherUserId")]
+        public virtual AuthUser VoucherUser { get; set; }
+
+        [Display(Name = "Discount Type")]
+        public ShoppingVoucherDiscountTypeEnum DiscountType { get; set; }
+
+        [Display(Name = "Discount")]
+        public decimal DiscountFigure { get; set; }
+
+        public DateTime? VoucherUsedDate { get; set; }
+        public DateTime? VoucherExpiryDate { get; set; }
+        public virtual ICollection<ShoppingVoucherUsage> OrderDetails { get; set; }
+    }
+
+    public class ShoppingVoucherUsage : BaseAuditInfo
+    {
+        [Key]
+        [Display(Name = "Voucher Usage Id")]
+        public int ShoppingVoucherUsageId { get; set; }
+        
+        public int ShoppingVoucherId { get; set; }
+        [ForeignKey("ShoppingVoucherId")]
+        public virtual ShoppingVoucher ShoppingVoucher { get; set; }
+
+        public int? VoucherUserId { get; set; }
+        [ForeignKey("VoucherUserId")]
+        public virtual AuthUser VoucherUser { get; set; }
+        public int? OrderId { get; set; }
+        [ForeignKey("OrderId")]
+        public virtual Order Order{ get; set; }
+
+    }
+}
---
+++ GaneEcommerce.Entities/Domain/ViewModels/AdyenPayments/ShoppingVoucherResponseModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Ganedata.Core.Entities.Enums;
+
+namespace Ganedata.Core.Models.AdyenPayments
+{
+
+    public class ShoppingVoucherValidationRequestModel
+    {
+        public string Username { get; set; }
+        public string VoucherCode { get; set; }
+        public int? UserId { get; set; }
+        public int? OrderId { get; set; }
+    }
+    public class ShoppingVoucherValidationResponseModel
+    {
+        public string VoucherCode { get; set; }
+        public decimal DiscountFigure { get; set; }
+        public int DiscountType { get; set; }
+
+        //Can be used to determine if expiry becomes an issue
+        public DateTime VerifiedTimestamp{ get; set; }
+        public int Status { get; set; }
+    }
+
+}
